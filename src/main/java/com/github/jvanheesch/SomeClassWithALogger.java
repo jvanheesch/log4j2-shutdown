2cf6acc11fffac13f1bbe10cead937565e3e4b42
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-09-21 20:14:07 +0200

    cleanup.

--- src/main/java/com/github/jvanheesch/SomeClassWithALogger.java
+++ src/main/java/com/github/jvanheesch/SomeClassWithALogger.java
@@ -3,12 +3,10 @@ package com.github.jvanheesch;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import javax.servlet.ServletContext;
-
 public class SomeClassWithALogger {
     private static final Log LOGGER = LogFactory.getLog(SomeClassWithALogger.class);
 
-    static void log(ServletContext sc) {
+    public static void log() {
         LOGGER.error("test");
     }
 }
