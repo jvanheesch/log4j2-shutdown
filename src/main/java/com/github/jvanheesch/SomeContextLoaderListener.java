88df6db8b3125ed4dcc4c24f1b6eab0f04ebd006
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-09-21 20:12:02 +0200

    cleanup.

--- src/main/java/com/github/jvanheesch/SomeContextLoaderListener.java
+++ src/main/java/com/github/jvanheesch/SomeContextLoaderListener.java
@@ -4,17 +4,10 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class SomeContextLoaderListener implements ServletContextListener {
-
-    public SomeContextLoaderListener() {
-    }
-
-
     public void contextInitialized(ServletContextEvent event) {
     }
-
 
     public void contextDestroyed(ServletContextEvent event) {
         SomeServletContextListener.log(event.getServletContext());
     }
-
 }
