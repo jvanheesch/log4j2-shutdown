362ba5b3ef6afaaa61112ef64a6fe80a1e4dc91b
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-09-23 19:00:48 +0200

    fixed name

--- src/main/java/com/github/jvanheesch/SomeServletContextListener.java
+++ src/main/java/com/github/jvanheesch/SomeServletContextListener.java
@@ -3,9 +3,9 @@ package com.github.jvanheesch;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-public class SomeContextLoaderListener implements ServletContextListener {
+public class SomeServletContextListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent event) {
-        System.out.println("SomeContextLoaderListener.contextInitialized");
+        System.out.println("SomeServletContextListener.contextInitialized");
     }
 
     public void contextDestroyed(ServletContextEvent event) {
